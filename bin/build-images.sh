3364c9cd90b1e6a02a9f5ba615d5384da2ec205a
Ruben Taelman <ruben.taelman@ugent.be>
2019-01-30 11:11:10 +0100

    Extend cache image with data

--- bin/build-images.sh
+++ bin/build-images.sh
@@ -10,6 +10,10 @@ docker build --quiet --file dockerfiles/server-initialized \
     --build-arg SERVER_CONFIG=$SERVER_CONFIG \
     -t $testname-server-initialized .
 
+# Build cache
+docker build --quiet --file dockerfiles/cache-initialized \
+    -t $testname-cache-initialized .
+
 # Build comunica client
 docker build --quiet --file dockerfiles/comunica-initialized \
     --build-arg CLIENT_CONFIG=$CLIENT_CONFIG \
