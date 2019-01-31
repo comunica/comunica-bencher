bf973579401da5b1621c12a9d922b35a4b0ab728
Ruben Taelman <ruben.taelman@ugent.be>
2019-01-31 11:04:38 +0100

    Add script for preparing dataset and queries

---
+++ prepare-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source .env
+
+# Generate dataset and queries
+docker run --rm -it -v $(pwd)/input/:/output comunica/watdiv -s $DATASET_SCALE -q $QUERY_COUNT
+
+# Convert dataset to HDT
+docker run --rm -it -v $(pwd)/input/:/output rdfhdt/hdt-cpp rdf2hdt /output/dataset.nt /output/dataset.hdt
+
+# Cleanup the unneeded .nt file
+rm $(pwd)/input.dataset.nt
