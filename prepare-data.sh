bf51b4d803971010f7b445055747fbbbab4a2b21
Ruben Taelman <ruben.taelman@ugent.be>
2019-01-31 13:13:23 +0100

    Also add HDT index file to server image

--- prepare-data.sh
+++ prepare-data.sh
@@ -5,7 +5,11 @@ source .env
 docker run --rm -it -v $(pwd)/input/:/output comunica/watdiv -s $DATASET_SCALE -q $QUERY_COUNT
 
 # Convert dataset to HDT
+rm $(pwd)/input/dataset.hdt.index.v1-1
 docker run --rm -it -v $(pwd)/input/:/output rdfhdt/hdt-cpp rdf2hdt /output/dataset.nt /output/dataset.hdt
+
+# Generate HDT index file
+docker run --rm -it -v $(pwd)/input/:/output rdfhdt/hdt-cpp hdtSearch /output/dataset.hdt -q 0
 
 # Cleanup the unneeded .nt file
 rm $(pwd)/input/dataset.nt
